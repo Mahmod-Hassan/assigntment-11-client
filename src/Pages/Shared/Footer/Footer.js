50e3a903cd9143b4b9506f495c4d34fbd2cf819e
Mahmod Hassan <mahmodhasan7788@gmail.com>
2022-11-08 22:01:29 +0600

    basic footer done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>this is footer</h2>
-        </div>
+        <footer className="footer p-10 bg-black text-white">
+            <div>
+                <span className="footer-title">Services</span>
+                <a href='/'>Branding</a>
+                <a href='/'>Design</a>
+                <a href='/'>Marketing</a>
+                <a href='/'>Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a href='/'>About us</a>
+                <a href='/'>Contact</a>
+                <a href='/'>Jobs</a>
+                <a href='/'>Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a href='/'>Terms of use</a>
+                <a href='/'>Privacy policy</a>
+                <a href='/'>Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
